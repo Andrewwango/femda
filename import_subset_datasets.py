cee27cca2cc05499da8b6814510c7cc9dba731e6
Andrew Wang <andrew.wang27@gmail.com>
2021-03-22 12:05:17 +0000

    Implemented LDA

--- import_subset_datasets.py
+++ import_subset_datasets.py
@@ -3,7 +3,7 @@ from sklearn import decomposition #PCA
 import random
 import pandas as pd
 
-def import_subset(dataset, labels, selected_cat, n_pca, n_sample):
+def import_subset(dataset, labels, selected_cat, n_pca, n_sample, pca=None):
     
     i = 1
     
@@ -22,9 +22,10 @@ def import_subset(dataset, labels, selected_cat, n_pca, n_sample):
         sample = random.sample(range(data_label.shape[0]), n_sample)
         subset_data = pd.concat([subset_data, data_label.iloc[sample, :]])
         subset_labels[(n_sample*(i-1)):(n_sample*i)] = cat  
-
-    pca = decomposition.PCA(n_components = n_pca)
-    pca.fit(subset_data)
+    
+    if pca is None:
+        pca = decomposition.PCA(n_components = n_pca)
+        pca.fit(subset_data)
 
     # returns true labels, even if it's noise
-    return pca.transform(subset_data), np.array(subset_labels).astype(int), subset_data, pca.explained_variance_ratio_
+    return pca.transform(subset_data), np.array(subset_labels).astype(int), subset_data, pca.explained_variance_ratio_, pca
