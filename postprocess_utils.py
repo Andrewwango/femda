f4db0f37667886a4479d1fcde2729824e3004b03
Andrew Wang <andrew.wang27@gmail.com>
2021-05-20 11:34:36 +0100

    reorganised folders

--- postprocess_utils.py
+++ postprocess_utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+from scipy.optimize import linear_sum_assignment as linear_sum_assignment
+
 
 def label_outliers_kth2(X_k, mean, cov, thres=0):
     diff = X_k - mean
@@ -43,3 +45,25 @@ def evaluate_estimators(model, true_means, true_covs):
     #print(means, true_means_list)
     true_covs_list = [true_covs[key] for key in sorted(true_covs.keys())]
     return errors_means(true_means_list, means), errors_covs(true_covs_list, covs)
+
+def acc(y_true, y_pred):
+    """
+    Calculate clustering accuracy. Require scikit-learn installed
+
+    # Arguments
+        y: true labels, numpy.array with shape `(n_samples,)`
+        y_pred: predicted labels, numpy.array with shape `(n_samples,)`
+
+    # Return
+        accuracy, in [0,1]
+    """
+    y_true = y_true.astype(np.int64)
+    assert y_pred.size == y_true.size
+    D = max(y_pred.max(), y_true.max()) + 1
+    w = np.zeros((D, D), dtype=np.int64)
+    for i in range(y_pred.size):
+        w[y_pred[i], y_true[i]] += 1
+    ind = linear_sum_assignment(w.max() - w)
+    ind = np.asarray(ind).T
+    
+    return sum([w[i, j] for i, j in ind]) * 1.0 / y_pred.size
--- preprocess_utils.py
+++ preprocess_utils.py
@@ -1,6 +1,38 @@
 import numpy as np
 from scipy import linalg
 from fit import *
+from sklearn import decomposition #PCA
+import random
+import pandas as pd
+
+def import_subset(dataset, labels, selected_cat, n_pca, n_sample, pca=None):
+    
+    i = 1
+    assert (n_pca > 0 or n_pca == -1)
+    
+    # n_sample from each category
+    subset_labels = np.zeros((n_sample*len(selected_cat), ))  
+
+    data_label = dataset.loc[selected_cat[0]==labels]
+    sample = random.sample(range(data_label.shape[0]), n_sample)
+    subset_data = data_label.iloc[sample, :]
+    subset_labels[(n_sample*(i-1)):(n_sample*i)] = selected_cat[0]    
+
+    for cat in selected_cat[1:]:
+        i += 1
+        data_label = dataset.loc[labels==cat]
+        sample = random.sample(range(data_label.shape[0]), n_sample)
+        subset_data = pd.concat([subset_data, data_label.iloc[sample, :]])
+        subset_labels[(n_sample*(i-1)):(n_sample*i)] = cat  
+    
+    if pca is None and n_pca != -1:
+        pca = decomposition.PCA(n_components = n_pca)
+        pca.fit(subset_data)
+    
+    return_data = np.array(subset_data) if n_pca == -1 else pca.transform(subset_data)
+
+    return return_data, np.array(subset_labels).astype(int), subset_data, None, pca
+
 
 def split(n, perc):
     a = int(np.floor(n*perc))
